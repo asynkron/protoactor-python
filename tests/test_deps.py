281b85f443c5936b99186aa0efd5c3b841b60c81
ipinak <giannis.pin@gmail.com>
2017-02-11 22:12:17 +0100

    New MailboxQueue * adds 'interface' for a mailbox queue * uses the default queue python implementation to build an UnboundedMailboxQueue * adds tests (which should increase test coverage)

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -6,3 +6,4 @@ def test_circular_dependencies():
     """Verify that there are no circular dependencies"""
     from protoactor.utils import *
     from protoactor.messages import *
+    from protoactor.mailbox import *
