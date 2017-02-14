06dd129e7aef4156ed36d3a4231ba14698a3b3bc
ipinak <giannis.pin@gmail.com>
2017-02-14 18:28:02 +0100

    Minor fixes & EventStream implementation
    * fixes minor issues
    * adds NotImplementedError to prohibit implementation of a few base
    classes
    * adds an EventStream, DeadLetterEvent
    * publish DeadLetterEvents when sending user and system messages with a
    DeadLettersProcess
    * adds tests for the EventStream, which verify pub-sub work both for
    DeadLetterEvents and other types of messages

--- protoactor/pid.py
+++ protoactor/pid.py
@@ -1,5 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 class PID(object):
-    def __init__(self, address, id, ref=None):
+
+    def __init__(self, address, id, aref=None):
         self.address = address
         self.id = id
-        self.ref = ref
+        self.aref = aref
+
+    def __str__(self):
+        """Generate a human readable string with the address and ID of
+        the PID."""
+
+        return "%(address)s/%(id)s" % {
+            "address": self._address,
+            "id": self._id
+        }
