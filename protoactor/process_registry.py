c86018889d3e034139c38cb3038d7996b1fd0ba1
alexpantyukhin <apantykhin@gmail.com>
2018-01-22 13:30:32 +0400

    small refactoring of process registry

--- protoactor/process_registry.py
+++ protoactor/process_registry.py
@@ -2,15 +2,14 @@ from multiprocessing import RLock
 
 from . import utils, pid as pid_, process
 
-
 @utils.singleton
 class ProcessRegistry:
-    def __init__(self, resolver=None, host: str = "nonhost") -> None:
-        self._hostResolvers = [resolver] if resolver is not None else []
+    def __init__(self) -> None:
+        self._hostResolvers = []
         # python dict structure is atomic for primitive actions. Need to be checked
         self.__local_actor_refs = {}
         self.__sequence_id = 0
-        self.__address = host
+        self.__address = "nonhost"
         self.__lock = RLock()
 
     @property
@@ -21,14 +20,16 @@ class ProcessRegistry:
     def address(self, address: str):
         self.__address = address
 
+    def register_host_resolver(self, resolver):
+        self._hostResolvers.append(resolver)
+
     def get(self, pid: 'PID') -> process.AbstractProcess:
-        if pid.address != self.__address:
+        if pid.address != "nonhost" and pid.address != self.__address:
             for resolver in self._hostResolvers:
                 reff = resolver(pid)
                 if reff is None:
                     continue
 
-                pid.process = reff
                 return reff
 
         ref = self.__local_actor_refs.get(pid.id, None)
