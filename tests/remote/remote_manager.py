eaa9c8a234903546b5790bc801e4a2eda461b75b
Valentin <compaq1219@mail.ru>
2019-08-02 20:02:34 +0300

    Refactoring + Router Subsystem

--- tests/remote/remote_manager.py
+++ tests/remote/remote_manager.py
@@ -9,10 +9,10 @@ class RemoteManager():
     def __init__(self):
         Serialization().register_file_descriptor(DESCRIPTOR)
         self.__nodes = {}
-        self.__default_node_address = '127.0.0.1:12000'
+        self.__default_node_address = 'localhost:12000'
 
-        self.provision_node('127.0.0.1', 12000)
-        Remote().start("127.0.0.1", 12001)
+        self.provision_node('localhost', 12000)
+        Remote().start("localhost", 12001)
 
     @property
     def default_node_address(self):
@@ -22,8 +22,8 @@ class RemoteManager():
     def nodes(self):
         return self.__nodes
 
-    def provision_node(self, host='127.0.0.1', port=12000):
-        process = subprocess.Popen(['python', './remote/node/node.py', '--host', str(host), '--port', str(port)],
+    def provision_node(self, host='localhost', port=12000):
+        process = subprocess.Popen(['python', './node/node.py', '--host', str(host), '--port', str(port)],
                                    stdout=subprocess.PIPE,
                                    stdin=subprocess.PIPE)
         address = '%s:%s' % (host, port)
